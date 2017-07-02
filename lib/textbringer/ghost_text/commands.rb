d1c532042acc4d3ad56ed6a7e20a820361b02817
Shugo Maeda <shugo@ruby-lang.org>
2017-07-03 07:59:08 +0900

    Add Textbringer::GhostText::WebSocketServer.

--- lib/textbringer/ghost_text/commands.rb
+++ lib/textbringer/ghost_text/commands.rb
@@ -3,7 +3,7 @@
 define_command(:ghost_text_start) do
   background do
     thin = Rack::Handler.get("thin")
-    thin.run(Textbringer::GhostText::App.new,
+    thin.run(Textbringer::GhostText::WebSocketServer.new,
              Host: CONFIG[:ghost_text_host],
              Port: CONFIG[:ghost_text_port]) do |server|
       server.silent = true
