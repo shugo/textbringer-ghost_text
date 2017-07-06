617d70b62fafbe3612366f706fae4ef0c1da551f
Shugo Maeda <shugo@ruby-lang.org>
2017-07-06 09:19:00 +0900

    Wait until a buffer and event handlers are prepared.

--- lib/textbringer/ghost_text/server.rb
+++ lib/textbringer/ghost_text/server.rb
@@ -27,56 +27,62 @@ module Textbringer
       def accept_client(env)
         ws = Faye::WebSocket.new(env, nil,
                                  ping: CONFIG[:ghost_text_ping_interval])
-
+        q = Queue.new
         next_tick do
-          buffer = Buffer.new_buffer("*GhostText*")
-          switch_to_buffer(buffer)
+          setup_buffer(ws)
+          q.push(:done)
+        end
+        q.pop
+        ws.rack_response
+      end
 
-          syncing_from_remote_text = false
+      def setup_buffer(ws)
+        buffer = Buffer.new_buffer("*GhostText*")
+        switch_to_buffer(buffer)
 
-          buffer.on_modified do
-            unless syncing_from_remote_text
-              pos = buffer.substring(0, buffer.point).size
-              data = {
-                "text" => buffer.to_s,
-                "selections" => [{ "start" => pos, "end" => pos }]
-              }
-              ws&.send(data.to_json)
-            end
+        syncing_from_remote_text = false
+
+        buffer.on_modified do
+          unless syncing_from_remote_text
+            pos = buffer.substring(0, buffer.point).size
+            data = {
+              "text" => buffer.to_s,
+              "selections" => [{ "start" => pos, "end" => pos }]
+            }
+            ws&.send(data.to_json)
           end
+        end
 
-          buffer.on_killed do
-            ws&.close
-          end
-          
-          ws.on :message do |event|
-            data = JSON.parse(event.data)
-            next_tick do
-              syncing_from_remote_text = true
-              begin
-                buffer.composite_edit do
-                  buffer.delete_region(buffer.point_min, buffer.point_max)
-                  buffer.insert(data["text"])
-                  pos = data["selections"]&.dig(0, "start")
-                  if pos
-                    byte_pos = data["text"][0, pos].bytesize
-                    buffer.goto_char(byte_pos)
-                  end
+        buffer.on_killed do
+          ws&.close
+        end
+
+        ws.on :message do |event|
+          data = JSON.parse(event.data)
+          next_tick do
+            syncing_from_remote_text = true
+            begin
+              buffer.composite_edit do
+                buffer.delete_region(buffer.point_min, buffer.point_max)
+                buffer.insert(data["text"])
+                pos = data["selections"]&.dig(0, "start")
+                if pos
+                  byte_pos = data["text"][0, pos].bytesize
+                  buffer.goto_char(byte_pos)
                 end
-              ensure
-                syncing_from_remote_text = false
               end
-            end
-          end
-          
-          ws.on :close do |event|
-            ws = nil
-            next_tick do
-              kill_buffer(buffer, force: true)
+            ensure
+              syncing_from_remote_text = false
             end
           end
         end
-        ws.rack_response
+
+        ws.on :close do |event|
+          ws = nil
+          next_tick do
+            kill_buffer(buffer, force: true)
+          end
+        end
       end
     end
   end
