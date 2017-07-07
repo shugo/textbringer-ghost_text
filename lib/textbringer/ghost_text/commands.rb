4195f96743daac21c7d349eb5735c6bdef59807f
Shugo Maeda <shugo@ruby-lang.org>
2017-07-07 10:40:10 +0900

    Add documentation.

--- lib/textbringer/ghost_text/commands.rb
+++ lib/textbringer/ghost_text/commands.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
-define_command(:ghost_text_start) do
+define_command(:ghost_text_start,
+               doc: "Start GhostText server") do
   host = CONFIG[:ghost_text_host]
   port = CONFIG[:ghost_text_port]
-  message("Start GhostText Server: http://#{host}:#{port}")
+  message("Start GhostText server: http://#{host}:#{port}")
   background do
     thin = Rack::Handler.get("thin")
     app = Rack::ContentLength.new(Textbringer::GhostText::Server.new)
